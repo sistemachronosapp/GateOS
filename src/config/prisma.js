42ed9aa9a4b84d67d5662eab7d406f2727c6d46d
LGEEEEEE <lgbalbinoserra@gmail.com>
2026-06-19 10:17:25 -0300

    adicionando prisma corretamente

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,14 +1,5 @@
 const { PrismaClient } = require('@prisma/client');
-const { Pool } = require('pg');
-const { PrismaPg } = require('@prisma/adapter-pg');
 
-const connectionString = process.env.DATABASE_URL;
-
-// Configura o Pool de conexões nativo do Postgres
-const pool = new Pool({ connectionString });
-
-// Conecta o adaptador do Postgres ao Prisma
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 module.exports = prisma;
